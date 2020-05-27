7010331b6d1eb586925847ee15854fb89618a80f
Hyunsu Kim <hyunsu.j.kim@gmail.com>
2020-05-27 09:37:45 -0500

    deleted unneeded pages

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,17 +1,11 @@
 import React, { Component } from "react";
 import "./App.css";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import LandingPage from "../Pages/LandingPage/LandingPage";
-import RepairmanProfilePage from "../Pages/RepairmanProfilePage/RepairmanProfilePage";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Router>
-          <Route exact path="/" component={LandingPage} />
-          <Route exact path="/repairman/:id" component={RepairmanProfilePage} />
-        </Router>
+        <h1>hello</h1>
       </div>
     );
   }
