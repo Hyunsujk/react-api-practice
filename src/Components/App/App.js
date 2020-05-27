b58d52b0fef0c8daa067034b4318bca0a76005e4
Hyunsu Kim <hyunsu.j.kim@gmail.com>
2020-05-27 10:51:06 -0500

    added google button to page

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import "./App.css";
+import GoogleBtn from "../googleBtn";
 
 class App extends Component {
   render() {
     return (
       <div>
         <h1>hello</h1>
+        <GoogleBtn />
       </div>
     );
   }
